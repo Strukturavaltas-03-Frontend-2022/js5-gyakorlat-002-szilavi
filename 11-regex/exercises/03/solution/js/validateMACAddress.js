b335ea366193460a76c3cf534b41777781923d22
k3csk3 <szilagyi.viktor89@gmail.com>
2022-09-20 08:57:36 +0200

    lefut

--- 11-regex/exercises/03/solution/js/validateMACAddress.js
+++ 11-regex/exercises/03/solution/js/validateMACAddress.js
@@ -1,1 +1,6 @@
+const validateMACAddress = (macAddress) => {
+  const pattern = /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/;
+  return macAddress.match(pattern) ? true : false;
+};
+
 export default validateMACAddress;
